2205ab3963ff1b304d66746893af3acd28d15a31
wind <987625922@qq.com>
2020-01-20 20:04:18 +0800

    提交

--- src/views/project/Login.vue
+++ src/views/project/Login.vue
@@ -1,16 +1,42 @@
 <template>
   <div>
-    <button type="button" @click="reg()">登录</button>
-    <button type="button">注册</button>
+    <form v-if="!isReg">
+      用户名：<input type="text" v-model="name">
+      密码: <input type="text" v-model="password">
+      <button type="button" @click="login()">登录</button>
+    </form>
+    <form v-else>
+      用户名: <input type="text" v-model="name">
+      密码: <input type="text" v-model="password">
+      再次输入密码: <input type="text">
+      <button type="button" @click="addUser()">注册</button>
+      <button type="button" @click="cancel()">注册</button>
+    </form>
   </div>
 </template>
 
 <script>
   export default {
     name: 'Login',
+    data () {
+      return {
+        isReg: false,
+        name: '',
+        password: '',
+        repeat: ''
+      }
+    },
     methods: {
-      reg () {
+      login () {
         this.$router.push('/home')
+      },
+      addUser () {
+        this.isReg = true
+        localStorage.setItem('name', this.name)
+        localStorage.setItem('password', this.password)
+      },
+      cancel () {
+        this.isReg = false
       }
     }
   }
