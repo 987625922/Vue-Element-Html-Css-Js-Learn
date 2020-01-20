2a0f46b6fe57cf8b600ebc91dc4e1cba578da62f
98762 <987625922@qq.com>
2020-01-20 22:11:21 +0800

    学完课程

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import Add from '../views/project/Add.vue'
 Vue.use(Router)
 
 export default new Router({
+  linkActiveClass: 'active',
   routes: [
     {
       path: '/',
--- src/views/project/Login.vue
+++ src/views/project/Login.vue
@@ -4,13 +4,14 @@
       用户名：<input type="text" v-model="name">
       密码: <input type="text" v-model="password">
       <button type="button" @click="login()">登录</button>
+      <button type="button" @click="toReg()">注册</button>
     </form>
     <form v-else>
       用户名: <input type="text" v-model="name">
-      密码: <input type="text" v-model="password">
-      再次输入密码: <input type="text">
+      密码: <input type="password" v-model="password">
+      再次输入密码: <input type="password" v-model="repeat">
       <button type="button" @click="addUser()">注册</button>
-      <button type="button" @click="cancel()">注册</button>
+      <button type="button" @click="cancel()">取消</button>
     </form>
   </div>
 </template>
@@ -18,7 +19,7 @@
 <script>
   export default {
     name: 'Login',
-    data () {
+    data() {
       return {
         isReg: false,
         name: '',
@@ -27,16 +28,31 @@
       }
     },
     methods: {
-      login () {
-        this.$router.push('/home')
+      login() {
+        if (localStorage.getItem("name") == this.name &&
+          localStorage.getItem("password") == this.password) {
+          this.$router.push('/home')
+        }else {
+          alert("账号密码不正确")
+        }
       },
-      addUser () {
-        this.isReg = true
-        localStorage.setItem('name', this.name)
-        localStorage.setItem('password', this.password)
+      addUser() {
+        if (this.password == this.repeat) {
+          this.isReg = true
+          localStorage.setItem('name', this.name)
+          localStorage.setItem('password', this.password)
+          this.name = ''
+          this.password = ''
+          this.isReg = false
+        }else {
+          alert("密码不一致")
+        }
       },
-      cancel () {
+      cancel() {
         this.isReg = false
+      },
+      toReg() {
+        this.isReg = true
       }
     }
   }
