4eef58791d5a609b4f4426bf60dba764faaf0741
wind <987625922@qq.com>
2020-01-21 14:07:07 +0800

    axios的使用

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,13 @@ import Router from 'vue-router'
 import Login from '../views/project/Login.vue'
 import Home from '../views/project/Home.vue'
 import Add from '../views/project/Add.vue'
+import Learn from '../views/LearnNavigation'
+import Basic from '../views/List'
+import GetList from '../views/project2/GetList'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
+Vue.use(VueAxios,axios);
 Vue.use(Router)
 
 export default new Router({
@@ -11,6 +17,21 @@ export default new Router({
   routes: [
     {
       path: '/',
+      name: 'navigation',
+      component: Learn
+    },
+    {
+      path: '/getlist',
+      name: 'getlist',
+      component: GetList
+    },
+    {
+      path: '/basic',
+      name: 'basic',
+      component: Basic
+    },
+    {
+      path: '/login',
       name: 'login',
       component: Login
     },
---
+++ src/views/LearnNavigation.vue
@@ -0,0 +1,40 @@
+<template>
+  <el-container>
+    <el-header>
+      <el-button type="button" @click="toLearnBasic()">基础</el-button>
+      <el-button type="button" @click="toProject()">一个列表和登录的小项目</el-button>
+      <el-button type="button" @click="toProject2()">网络获取数据展示</el-button>
+    </el-header>
+    <el-main></el-main>
+  </el-container>
+</template>
+
+<script>
+  export default {
+    name: 'LearnNavigation',
+    methods: {
+      toLearnBasic () {
+        this.$router.push('basic')
+      },
+      toProject () {
+        this.$router.push('login')
+      },
+      toProject2 () {
+        this.$router.push('getlist')
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .el-main {
+    background-color: #ffffff;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
+  .el-header, .el-footer {
+    color: #333;
+    line-height: 60px;
+  }
+</style>
