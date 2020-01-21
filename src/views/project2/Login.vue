56ce0ccac5e96ecfee420a9f707f1ccf8754cb15
wind <987625922@qq.com>
2020-01-21 16:03:14 +0800

    登录界面

--- src/views/project2/Login.vue
+++ src/views/project2/Login.vue
@@ -1,110 +1,96 @@
 <template>
-  <div class="login-wrap">
-    <div class="ms-login">
-      <div class="ms-title">后台管理系统</div>
-      <el-form :model="param" :rules="rules" ref="login" label-width="0px" class="ms-content">
-        <el-form-item prop="username">
-          <el-input v-model="param.username" placeholder="username">
-            <el-button slot="prepend" icon="el-icon-lx-people"></el-button>
-          </el-input>
+  <div id="login_main">
+    <div>
+      <el-form ref="loginForm" :model="form" :rules="rules" label-width="80px" class="login-box">
+        <h3 class="login-title">欢迎登录</h3>
+        <el-form-item label="账号" prop="username">
+          <el-input type="text" placeholder="请输入账号" v-model="form.username"/>
         </el-form-item>
-        <el-form-item prop="password">
-          <el-input
-            type="password"
-            placeholder="password"
-            v-model="param.password"
-            @keyup.enter.native="submitForm()"
-          >
-            <el-button slot="prepend" icon="el-icon-lx-lock"></el-button>
-          </el-input>
+        <el-form-item label="密码" prop="password">
+          <el-input type="password" placeholder="请输入密码" v-model="form.password"/>
         </el-form-item>
-        <div class="login-btn">
-          <el-button type="primary" @click="submitForm()">登录</el-button>
-        </div>
-        <p class="login-tips">Tips : 用户名和密码随便填。</p>
+        <el-form-item>
+          <el-button type="primary" v-on:click="onSubmit('loginForm')">登录</el-button>
+        </el-form-item>
       </el-form>
+
+      <el-dialog
+        title="温馨提示"
+        :visible.sync="dialogVisible"
+        width="30%"
+        :before-close="handleClose">
+        <span>请输入账号和密码</span>
+        <span slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+      </span>
+      </el-dialog>
     </div>
   </div>
 </template>
 
 <script>
   export default {
-    data: function () {
+    name: 'Login',
+    data () {
       return {
-        param: {
-          username: 'admin',
-          password: '123123',
+        form: {
+          username: '',
+          password: ''
         },
+        // 表单验证，需要在 el-form-item 元素中增加 prop 属性
         rules: {
-          username: [{ required: true, message: '请输入用户名', trigger: 'blur' }],
-          password: [{ required: true, message: '请输入密码', trigger: 'blur' }],
+          username: [
+            { required: true, message: '账号不可为空', trigger: 'blur' }
+          ],
+          password: [
+            { required: true, message: '密码不可为空', trigger: 'blur' }
+          ]
         },
+
+        // 对话框显示和隐藏
+        dialogVisible: false
       }
     },
     methods: {
-      submitForm () {
-        // this.$refs.login.validate(valid => {
-        //   if (valid) {
-        //     this.$message.success('登录成功')
-        //     localStorage.setItem('ms_username', this.param.username)
-        //     this.$router.push('/')
-        //   } else {
-        //     this.$message.error('请输入账号和密码')
-        //     console.log('error submit!!')
-        //     return false
-        //   }
-        // })
-      },
-    },
+      onSubmit (formName) {
+        // 为表单绑定验证功能
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            // 使用 vue-router 路由到指定页面，该方式称之为编程式导航
+            this.$router.push('/main')
+          } else {
+            this.dialogVisible = true
+            return false
+          }
+        })
+      }
+    }
   }
 </script>
 
-<style scoped>
-  .login-wrap {
-    position: relative;
-    width: 100%;
+<style lang="scss" scoped>
+  #login_main {
     height: 100%;
+    padding: 100px 100px 0;
+    box-sizing: border-box;
+    position: relative;
     background-image: url(../../assets/img/bg_gihub_windows.jpg);
-    background-size: 100%;
   }
 
-  .ms-title {
-    width: 100%;
-    line-height: 50px;
-    text-align: center;
-    font-size: 20px;
-    color: #fff;
-    border-bottom: 1px solid #ddd;
-  }
-
-  .ms-login {
-    position: absolute;
-    left: 50%;
-    top: 50%;
+  .login-box {
+    border: 1px solid #DCDFE6;
     width: 350px;
-    margin: -190px 0 0 -175px;
+    margin: 180px auto;
+    padding: 35px 35px 15px 35px;
     border-radius: 5px;
-    background: rgba(255, 255, 255, 0.3);
-    overflow: hidden;
+    -webkit-border-radius: 5px;
+    -moz-border-radius: 5px;
+    box-shadow: 0 0 25px #909399;
   }
 
-  .ms-content {
-    padding: 30px 30px;
-  }
-
-  .login-btn {
+  .login-title {
     text-align: center;
-  }
-
-  .login-btn button {
-    width: 100%;
-    height: 36px;
-    margin-bottom: 10px;
-  }
-
-  .login-tips {
-    font-size: 12px;
-    line-height: 30px;
-    color: #fff;
+    margin: 0 auto 40px auto;
+    color: #303133;
   }
 </style>
